2979f09e3b27ea9589cebbf74629c9bfc63eea0d
RafaelvBossio <rafaelvargasbossio99@gmail.com>
2022-02-22 22:32:00 -0500

    Estructura y menu creados

---
+++ assets/js/index.js
@@ -0,0 +1,4 @@
+var contenido_logo = document.querySelector(".logo__imagen");
+contenido_logo.addEventListener("click", function(event){
+    document.getElementById('sobreMi').scrollIntoView();
+});
