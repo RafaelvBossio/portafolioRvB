350c6f3756d2cc845a0ad8888d76bffa1e38942a
RafaelvBossio <rafaelvargasbossio99@gmail.com>
2022-02-26 18:24:00 -0500

    implementando la responsividad del nav

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,28 @@
 var contenido_logo = document.querySelector("#arriba");
-contenido_logo.addEventListener("click", function(event){
-    document.getElementById('header').scrollIntoView();
+contenido_logo.addEventListener("click", function (event) {
+  document.getElementById("header").scrollIntoView();
 });
 
-var p = document.querySelector("#p");
-console.log(p.textContent.length)
+var mediaqueryTable = window.matchMedia("(min-width: 768px)");
+var mediaqueryPc = window.matchMedia("(min-width: 1024px)");
+
+var navVisible = document.querySelector("#navVisible");
+var btnArriba = document.querySelector(".arriba");
+var logo__titulo = document.querySelector("#logo__titulo");
+var navContacto = document.querySelector("#navContacto");
+
+/* Queries Para Table */
+if (mediaqueryTable.matches) {
+  btnArriba.classList.remove("invisible");
+  logo__titulo.classList.remove("invisible");
+  navContacto.classList.add("visible");
+  navVisible.classList.add("invisible");
+} else {
+}
+
+/* Queries Para Laptop */
+if (mediaqueryPc.matches) {
+  navVisible.classList.remove("invisible");
+  navContacto.classList.add("invisible");
+} else {
+}
