b465ca4f6038655edbe33d866940e04e25d246b8
RafaelvBossio <rafaelvargasbossio99@gmail.com>
2022-03-09 11:27:17 -0500

    Seccion de contactos terminada y btn CV terminado

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,3 +2,10 @@ var contenido_logo = document.querySelector("#arriba");
 contenido_logo.addEventListener("click", function (event) {
   document.getElementById("header").scrollIntoView();
 });
+
+let nombre = "rafael Bossio";
+
+function nombre () {
+  console.log(nombre);
+  return "hola";
+}
