771f7e98e2887874771522735ec8a039bf5e1fb6
RafaelvBossio <rafaelvargasbossio99@gmail.com>
2022-04-12 18:04:40 -0500

    terminado v12.04.22

---
+++ assets/js/certificados.js
@@ -0,0 +1,7 @@
+const cert1 = document.querySelector("#cert1");
+const cert2 = document.querySelector("#cert2");
+const cert3 = document.querySelector("#cert3");
+
+function pantallaCompleta(url){
+    let win = window.open(url, '_blank');
+}
